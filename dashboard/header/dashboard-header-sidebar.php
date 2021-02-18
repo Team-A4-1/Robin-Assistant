851f40f97761af42b33879cdca498613d6a45df8
florianmac <nairolf3>
2021-02-18 14:54:37 +0100

    file structure

--- dashboard/header/dashboard-header-sidebar.php
+++ dashboard/header/dashboard-header-sidebar.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="Author" content="Kevin Kranenburg" />
     <title>Dashboard</title>
-    <link rel="stylesheet" href="dashboard-home.css" />
+    <link rel="stylesheet" href="../dashboard/header/dashboard-header-sidebar.css" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
   </head>
   <body>
 
     <div class="navbar">
-        <img class="header-logo" src="../../assets/robin-bird-nobg.png" alt="Logo">
+        <img class="header-logo" src="../assets/robin-bird-nobg.png" alt="Logo">
         <a class="fas fa-lg fa-icon fa-chevron-down right"></a>
         <a href="#" class="right">Username</a>
         <a class="fas fa-lg fa-icon fa-user-circle right"></a>
@@ -22,13 +22,13 @@
 
     <!-- SideBar -->
     <div class="sidenav">
-            <img class="dashboard-icon" src="../../assets/dashboard-black-18dp.svg" alt="dashboard-icon" width="60px" height="60px">
+            <img class="dashboard-icon" src="../assets/dashboard-black-18dp.svg" alt="dashboard-icon" width="60px" height="60px">
         <a href="#dashboard">Dashboard</a>
         <hr>
-            <img class="patienten-logo" src="../../assets/account_box-black-18dp.svg" alt="patienten-logo" width="60px" height="60px">
+            <img class="patienten-logo" src="../assets/account_box-black-18dp.svg" alt="patienten-logo" width="60px" height="60px">
         <a href="#patienten">Patienten</a>
         <hr>
-            <img class="kalender-logo" src="../../assets/date_range-black-18dp.svg" alt="kalender-logo" width="60px" height="60px">
+            <img class="kalender-logo" src="../assets/date_range-black-18dp.svg" alt="kalender-logo" width="60px" height="60px">
         <a href="#kalender">Kalender</a>
         <hr>
       </div>
--- dashboard/kalender.php
+++ dashboard/kalender.php
@@ -5,4 +5,4 @@
  * Date: 18/02/2021
  * Time: 14:19
  */
-include "dashboard-header-sidebar.php";
+include "header/dashboard-header-sidebar.php";
