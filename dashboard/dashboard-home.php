544a99b583f041f0d7758d03a2e077ad8d4aa79f
kevinkranenburg <25953@ma-web.nl>
2021-02-19 14:27:23 +0100

    dashboard home added

---
+++ dashboard/dashboard-home.php
@@ -0,0 +1,12 @@
+<?php
+include "header/dashboard-header-sidebar.php";
+?>
+
+	<link rel="stylesheet" href="dashboard-home.css" />
+<div class="crumb">
+<h1 class="title-home'">Dashboard</h1>
+	<div class="crumbs crumbhome"></div>
+</div>
+
+<?php
+include "header/dashboard-footer.php";
